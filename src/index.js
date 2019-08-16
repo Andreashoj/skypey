8bf262c6f825048acd966af17a01023a2f5b3ea8
Andreas Hoj <andrewhoj@gmail.com>
2019-08-16 11:28:26 +0200

    firt commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./containers/App";
+import store from "./store";
+
+const render = () => {
+  fancyLog();
+  return ReactDOM.render(<App />, document.getElementById("root"));
+};
+
+function fancyLog() {
+  console.log("%c Rendered with 👉 👉👇", "background: purple; color: #FFF;");
+  console.log(store.getState());
+}
+
+render();
+
+store.subscribe(render);
